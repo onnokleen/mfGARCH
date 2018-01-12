b11b9737cd5fd780c92f6c22fdd50084079c1fc9
onnokleen <mail@onnokleen.de>
2018-01-12 10:58:59 +0100

    update

--- R/simulate_mfgarch_diffusion.R
+++ R/simulate_mfgarch_diffusion.R
@@ -49,7 +49,7 @@ simulate_mfgarch_diffusion <- function(n.days, mu, alpha, beta, gamma, m, theta,
 
   n.intraday <- 288
 
-  n.days <- n.days + low.freq * 300
+  n.days <- n.days + low.freq * K * 2
 
   n.sampling = 20 # 20 trades per 5 minutes
   delta <- n.intraday * n.sampling

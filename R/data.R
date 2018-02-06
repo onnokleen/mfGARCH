b7e1baf8936bd8a34d33f60842f4c7ce639bf5d9
onnokleen <mail@onnokleen.de>
2018-02-06 11:24:01 +0100

    data documentation improved

--- R/data.R
+++ R/data.R
@@ -1,15 +1,17 @@
-#' Prices of 50,000 round cut diamonds.
+#' Stock returns and financial conditions.
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' A dataset containing the S&P 500 stock returns and the NFCI
 #'
-#' @format A data frame with 11,644 rows and 21 variables:
+#' @format A data frame with 11,306 rows and 5 variables:
 #' \describe{
 #'   \item{date}{date}
 #'   \item{return}{daily S&P 500 log returns times 100}
-#'   \item{nfci}{daily S&P 500 log returns times 100}
-#'   \item{vix_sqrt_252}{de-annualized VIX}
-#'   \item{year_week}{a dummy for each year/week combination}
+#'   \item{rv}{5-minute realized variances}
+#'   \item{week}{a dummy for each year/week combination}
+#'   \item{nfci}{National Financial Conditions Index}
 #' }
 #' @source \url{https://github.com/onnokleen/mfGARCH/}
+#' @source \url{https://de.finance.yahoo.com/}
+#' @source \url{https://fred.stlouisfed.org/series/NFCI}
+#' @source \url{https://realized.oxford-man.ox.ac.uk}
 "df_financial"
